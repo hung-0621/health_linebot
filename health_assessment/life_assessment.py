ca2d4c59c2007a3b2a42ebe5ad823c96807faaa9
hung-0621 <kenny0909362798@gmail.com>
2024-05-31 16:46:39 +0800

    update

--- health_assessment/life_assessment.py
+++ health_assessment/life_assessment.py
@@ -6,4 +6,7 @@ from linebot.models import *
 class life(health_assessment):
     def __init__(self, user_id: str):
         super().__init__(user_id)
-        
+        self.life_title = self.title[7:12]
+        self.life_answer = self.answer[7:12]
+        self.life_correct_answer = self.correct_answer[6:11]
+        self.life_respond = self.respond[6:11]
